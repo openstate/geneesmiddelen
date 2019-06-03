8bf40b22c7e75772d71bcdba26526e4296f349ca
Breyten Ernsting <bje@dds.nl>
2019-06-03 16:11:54 +0200

    Basically know how to read out now ...

--- bin/parse.py
+++ bin/parse.py
@@ -7,17 +7,29 @@ from glob import glob
 from pprint import pprint
 
 import requests
+from lxml import etree
 
 def get_files():
     return glob('../data/*.html')
 
 def _parse_file(file_path):
-    pass
-
+    with open(file_path) as in_file:
+        content = in_file.read()
+    html = etree.HTML(content)
+    tables = html.xpath('//div[@class="table--container"]')
+    result = []
+    n = 0
+    for t in tables:
+        for r in t.xpath('./table/tbody/tr'):
+            # if u''.join(r.xpath('.//td//text()')).strip() == 'Productgroep':
+            cell_text = u''.join(r.xpath('./td[1]//text()')).strip()
+            if cell_text == 'Productgroep':
+                n += 1
+    print(n)
 
 def main(argv):
     file_paths = get_files()
-    for file_path in file_paths:
+    for file_path in sorted(file_paths):
         print(file_path)
         _parse_file(file_path)
     return 0
