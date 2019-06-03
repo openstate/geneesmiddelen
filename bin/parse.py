8f6a4066db6e5046a2f0c9f17b6c0b318cd58fcf
Breyten Ernsting <bje@dds.nl>
2019-06-03 16:51:11 +0200

    Actually parse this.

--- bin/parse.py
+++ bin/parse.py
@@ -18,14 +18,50 @@ def _parse_file(file_path):
     html = etree.HTML(content)
     tables = html.xpath('//div[@class="table--container"]')
     result = []
+    cur = None
     n = 0
+    p = 0
     for t in tables:
         for r in t.xpath('./table/tbody/tr'):
             # if u''.join(r.xpath('.//td//text()')).strip() == 'Productgroep':
             cell_text = u''.join(r.xpath('./td[1]//text()')).strip()
             if cell_text == 'Productgroep':
                 n += 1
-    print(n)
+                if cur is not None:
+                    result.append(cur)
+                cur = {
+                    'Productgroep': None,
+                    'Maximumprijs': None,
+                    'Eenheid': None,
+                    'Artikelen': []
+                }
+                p = 0
+                continue
+            if cur is None:
+                continue
+            if p == 0:  # Productgroep
+                cur['Productgroep'] = cell_text
+                p += 1
+            if p == 1:  #Maximumprijs
+                cell_text = u''.join(r.xpath('./td[2]//text()')).strip()
+                if ' per ' in cell_text:
+                    price, unit = cell_text.split('per', 1)
+                else:
+                    unit = 'Onbekend'
+                    price = cell_text.replace(' per', '')
+                cur['Maximumprijs'] = price.strip()
+                cur['Eenheid'] = unit.strip()
+                p += 1
+                continue
+            if p == 2 and cell_text == 'Registratienummer':
+                p += 1
+                continue
+            if p == 3 and cell_text != '':
+                cur['Artikelen'].append({
+                    'Registratienummer': cell_text,
+                    'Artikelnaam': u''.join(r.xpath('./td[2]//text()')).strip()
+                })
+    pprint((n, len(result),))
 
 def main(argv):
     file_paths = get_files()
